30ca197c4995bb4dbaa32e97c4ee8876f7f69ef6
Half <72697591+Ha1fCheese@users.noreply.github.com>
2023-05-15 18:43:28 +0300

    1.3.12

--- javascript/btnfontsize.js
+++ javascript/btnfontsize.js
@@ -1,11 +1,11 @@
-let num = 0;
 function increaseFontSize() {
     let preTag = document.getElementsByTagName("pre")[0];
-    let currentFontSize = parseFloat(window.getComputedStyle(preTag).fontSize);
-    preTag.style.fontSize = (currentFontSize +1) + "px";
+    let currentFontSize = parseInt(window.getComputedStyle(preTag).fontSize);
+    preTag.style.fontSize = (currentFontSize + 1) + "px";
+
 }
 function decreaseFontSize() {
     let preTag = document.getElementsByTagName("pre")[0];
-    let currentFontSize = parseFloat(window.getComputedStyle(preTag).fontSize);
-    preTag.style.fontSize = (currentFontSize -1) + "px";
+    let currentFontSize = parseInt(window.getComputedStyle(preTag).fontSize);
+    preTag.style.fontSize = (currentFontSize - 1) + "px";
 }
