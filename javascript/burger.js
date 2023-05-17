9e118f0fd6dd4741c7bc4189743ede2e52825ef6
Half <72697591+Ha1fCheese@users.noreply.github.com>
2023-05-18 02:43:54 +0300

    1.3.14

--- javascript/burger.js
+++ javascript/burger.js
@@ -35,6 +35,6 @@ const versionControl = document.getElementsByTagName('footer')[0];
 let div = document.createElement('div');
 div.className = 'footer__div'
 let v = document.createElement('p');
-v.innerHTML = 'Content release 1.3.13';
+v.innerHTML = 'Content release 1.3.14';
 div.appendChild(v);
 versionControl.appendChild(div);
